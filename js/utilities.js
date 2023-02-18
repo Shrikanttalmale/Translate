a6c46837ff7e0952cf12fc2f9ca44a5260599849
Shrikant <shrikanttalmale@yahoo.co.in>
2023-02-19 00:50:16 +0530

    Update utilities.js

--- js/utilities.js
+++ js/utilities.js
@@ -18,3 +18,38 @@ function copyText() {
     $('#txtInputLang').val('');
     $('#txtOutputLang').val('');
   });
+
+
+  $('.hindiTxtbox').on('keypress', function(event) {
+    let keycode = (event.keyCode ? event.keyCode : event.which);
+    if(keycode == '32') {
+     // $.toastr.success('Space key pressed');
+
+     var words = $('.hindiTxtbox').val().split(' ');
+     var lastWord = words[words.length - 1];
+
+        if(lastWord!='')
+        {
+          var urlHindi = 'https://www.google.com/inputtools/request?text='+ lastWord +'&itc=hi-t-i0-und&num=1&cp=0&cs=1&ie=utf-8&oe=utf-8&app=demopage'
+
+          $.ajax({
+            url: urlHindi,
+            type: 'GET',
+            //data: jQuery.param({ source_lang: "en", target_lang : "hi",	text : "how are you my friend"}) ,
+            //data: jQuery.param({ source_lang: sourceLang, target_lang : targetLang,	text : text}) ,
+            contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+            success: function (response) {
+              var translatedText = response[1][0][1][0];
+
+            var newText = $("textarea.hindiTxtbox").val().replace(lastWord, translatedText);
+            $("textarea.hindiTxtbox").val(newText);
+             
+
+            },
+            error: function () {
+              //$.toastr.error('error converting');
+            }
+          }); 
+        }
+    }
+  });
